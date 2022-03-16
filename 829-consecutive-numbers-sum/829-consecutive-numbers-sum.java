9bab6b43b618bbe3332727f3e05fe7f867e83cb6
shreyash-cmu <80502810+shreyash-cmu@users.noreply.github.com>
2022-03-15 23:33:05 -0400

    Time: 5 ms (74.19%), Space: 41.7 MB (5.85%) - LeetHub

---
+++ 829-consecutive-numbers-sum/829-consecutive-numbers-sum.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int consecutiveNumbersSum(int n) {
+        /*if(n==1||n==2) return 1;
+        int start = 1;
+        int m = n/2 + 1;
+        int runningSum =0;
+        int ansCnt =0;
+        for(int i=1;i<=m;i++){
+            runningSum += i;
+            if(runningSum==n){
+                ansCnt++;
+                runningSum -= start++;
+            }else if(runningSum > n){
+                while(runningSum > n){
+                    runningSum -= start++;
+                }
+                
+                if(runningSum==n) ansCnt++;
+            }
+            
+        }
+        return ansCnt+1;*/
+        /*
+            runningSum = N;
+            keep on subtracting i form the runningSum;
+            if runningSum % i ===0 then incresase count
+            else break;
+            
+            runningsSum = 9
+            i = 1
+            
+            
+            runningSum = 7  
+            
+            
+        
+        */
+        int ans=0;
+        int windowSize=1;
+        boolean isWindowSizePossible=true;
+        int runningSum=0;
+        int i=0;
+        while(runningSum<=n){
+            runningSum +=i++;
+            int temp= n-runningSum;
+            if(temp !=0 && temp%windowSize==0){
+            ans++;
+            }
+            windowSize++;
+           
+            
+        }
+        return ans;
+    }
+}
