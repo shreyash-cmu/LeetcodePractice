705c3e2274bcd6d0fcd3c53b7485642763da864f
shreyash-cmu <80502810+shreyash-cmu@users.noreply.github.com>
2022-03-31 01:14:23 -0400

    Time: 0 ms (100.00%), Space: 41.8 MB (20.22%) - LeetHub

--- 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.java
+++ 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.java
@@ -1,5 +1,6 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
+        /*Chaining Technique to measure the impact of largest element till the index*/
         int max = Integer.MIN_VALUE;
         int chunks =0;
         for(int i=0;i<arr.length;i++){
