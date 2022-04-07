358f2ec392998cb60b2cacc3b2d88e168976527e
shreyash-cmu <80502810+shreyash-cmu@users.noreply.github.com>
2022-04-07 15:05:46 -0400

    Time: 7 ms (44.59%), Space: 45 MB (39.65%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -5,25 +5,15 @@ class Solution {
         return new ArrayList<>(ret);
     }
     void backTrack(int[] nums,int idx, int target, List<Integer> levels){
-        //System.out.println(levels);
-        //System.out.println("SUMIS: "+ currentSum);
         if(target<0) return;
         if(target==0){
-            //Collections.sort(levels);
-            //System.out.println(levels);
-            //if(!ret.contains(levels)){
-                 ret.add(new ArrayList<>(levels));
-            //}
-           
+            ret.add(new ArrayList<>(levels));
             return;
         }
         for(int i = idx;i<nums.length;i++){
             levels.add(nums[i]);
-            //currentSum += nums[i];
             backTrack(nums,i,target-nums[i], levels);
-            //currentSum -= nums[i];
             levels.remove(levels.size()-1);
         }
-        
     }
 }
