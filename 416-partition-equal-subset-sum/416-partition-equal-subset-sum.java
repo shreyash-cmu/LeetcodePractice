b8e5bf2b2bf811cd4c778f508a247e78d115f8a9
shreyash-cmu <80502810+shreyash-cmu@users.noreply.github.com>
2022-04-14 10:35:09 -0400

    Time: 246 ms (8.52%), Space: 42.8 MB (82.55%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -5,7 +5,7 @@ class Solution {
             sum+= num;
         }
         if(sum%2==1) return false;
-        
+        int target = sum/2;
         Set<Integer> set = new HashSet<>();
         set.add(0);
         for(int num:nums){
@@ -13,9 +13,12 @@ class Solution {
             for(int n:set){
                 levelSet.add(n);
                 levelSet.add(n+num);
+                if(levelSet.contains(target)){
+                    return true;
+                }
             }
             set = new HashSet<Integer>(levelSet);
         }
-        return set.contains(sum/2);
+        return set.contains(target);
     }
 }
