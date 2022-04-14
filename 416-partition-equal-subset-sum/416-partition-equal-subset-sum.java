c9d34099fa84f27a7a830bcf5f238e98d69e3ca6
shreyash-cmu <80502810+shreyash-cmu@users.noreply.github.com>
2022-04-14 10:34:13 -0400

    Time: 847 ms (5.01%), Space: 44.2 MB (70.22%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int sum = 0;
+        for(int num: nums){
+            sum+= num;
+        }
+        if(sum%2==1) return false;
+        
         Set<Integer> set = new HashSet<>();
         set.add(0);
         for(int num:nums){
-            sum += num;
             Set<Integer> levelSet = new HashSet<>();
             for(int n:set){
                 levelSet.add(n);
                 levelSet.add(n+num);
             }
             set = new HashSet<Integer>(levelSet);
-            //System.out.println(set);
         }
-        if(sum%2==1) return false;
         return set.contains(sum/2);
     }
 }
