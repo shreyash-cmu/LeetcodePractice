6cc668594283a302970b6bed14126916c8fd8060
shreyash-cmu <80502810+shreyash-cmu@users.noreply.github.com>
2022-05-02 14:18:05 -0400

    Time: 147 ms (5.98%), Space: 124.3 MB (65.59%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,60 +1,56 @@
 class LRUCache {
-    
-    class Node {
+    class Node{
         int key;
         int value;
         Node prev;
         Node next;
     }
-    private void addNode(Node node){
-        node.next = head.next;
-        node.prev = head;
-        head.next.prev = node;
-        head.next = node;
-    }
-    private void removeNode(Node node){
-        node.prev.next = node.next;
-        node.next.prev = node.prev;
-    }
-    private void moveToHead(Node node){
-        removeNode(node);
-        addNode(node);
-    }
-    private Node popTail(){
-        Node ret = tail.prev;
-        removeNode(ret);
-        return ret;
-    }
-    
+    Node head;
+    Node tail;
     Map<Integer,Node> cache;
-    private int size;
-    private final int CAPACITY;
-    private Node head,tail;
+    int size;
+    int totalCapacity;
 
     public LRUCache(int capacity) {
-        CAPACITY = capacity;
+        totalCapacity = capacity;
         cache = new HashMap<>();
         head = new Node();
         tail = new Node();
-        tail.prev = head;
         head.next = tail;
+        tail.prev = head;
+    }
+    public void addNode(Node node){
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+    public void removeNode(Node node){
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+    public Node popTail(){
+        Node remove = tail.prev;
+        removeNode(remove);
+        return remove;
+    }
+    public void moveToHead(Node n){
+        removeNode(n);
+        addNode(n);
     }
     
     public int get(int key) {
-        //System.out.println(cache);
         Node n = cache.get(key);
-        if(n==null){
-            return -1;
-        }
+        if(n==null)return -1;
         moveToHead(n);
         return n.value;
     }
     
     public void put(int key, int value) {
-        Node curr = cache.get(key);
-        if(curr!=null){
-            curr.value = value;
-            moveToHead(curr);
+        Node n = cache.get(key);
+        if(n!=null){
+            n.value = value;
+            moveToHead(n);
         }else{
             Node newNode = new Node();
             newNode.key = key;
@@ -62,31 +58,14 @@ class LRUCache {
             addNode(newNode);
             cache.put(key,newNode);
             ++size;
-            if(size>CAPACITY){
-                Node removed = popTail();
-                cache.remove(removed.key);
+            if(size>totalCapacity){
+                Node ret = popTail();
+                cache.remove(ret.key);
                 --size;
             }
         }
-        
     }
 }
-
-
-//HASHmAP (key, vaLues)
-//qUEUE (KEY)
-
-
-//iNSERT hASHMAP (KEY,VALUE)
-//lru aRRaylist <KEY>
-//
-
-//GET -> hASHmAP ()
-//HASHMAP.GET(KEY)
-//LRU.REMOVE()
-
-
-
 
 /**
  * Your LRUCache object will be instantiated and called as such:
